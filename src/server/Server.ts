79e4bf9ae52ee446fcf924cb77be8d65fa0da3c4
CodeSandbox <support@codesandbox.io>
2023-10-17 19:18:46 +0000

    Sandbox snapshot commit

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import "dotenv/config";
 import { router } from "./routes";
+import TranslationsYup from "./shared/services/TranslationsYup";
 
 const server = express();
 
--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -4,23 +4,31 @@ import * as yup from "yup";
 
 interface ICidade {
   nome: string;
+  estado: string;
 }
 
 const bodyValidation: yup.Schema<ICidade> = yup.object().shape({
   nome: yup.string().required().min(3),
+  estado: yup.string().required().min(3),
 });
 
 export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
   let validatedData: ICidade | undefined = undefined;
   try {
-    validatedData = await bodyValidation.validate(req.body);
-  } catch (error) {
-    const yupError = error as yup.ValidationError;
+    validatedData = await bodyValidation.validate(req.body, {
+      abortEarly: false,
+    });
+  } catch (err) {
+    const yupError = err as yup.ValidationError;
+    const errors: Record<string, string> = {};
 
-    return res.json({
-      errors: {
-        default: yupError.message,
-      },
+    yupError.inner.forEach((error) => {
+      if (error.path === undefined) return;
+      errors[error.path] = error.message;
+    });
+
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      errors,
     });
   }
 
