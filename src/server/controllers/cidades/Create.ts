b25178955a444ff99844f7d50696def6a3d0845b
CodeSandbox <support@codesandbox.io>
2023-10-25 14:29:03 +0000

    Initial commit

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,38 +1,40 @@
 import { Request, Response } from "express";
-import { StatusCodes } from "http-status-codes";
 import * as yup from "yup";
+import { validation } from "../../shared/middleware";
+
+//bodyValidation (middleware)
 
 interface ICidade {
   nome: string;
   estado: string;
 }
 
-const bodyValidation: yup.Schema<ICidade> = yup.object().shape({
-  nome: yup.string().required().min(3),
-  estado: yup.string().required().min(3),
-});
+//queryValidation (middleware)
+
+interface IFilter {
+  filter?: string;
+  limit?: number;
+}
+
+//O argumento de validation é passado como função para que seja possível definir a tipagem dos objetos
+export const createValidator = validation((getSchema) => ({
+  body: getSchema<ICidade>(
+    yup.object().shape({
+      nome: yup.string().required().min(3),
+      estado: yup.string().required().min(3),
+    }),
+  ),
+  query: getSchema<IFilter>(
+    yup.object().shape({
+      filter: yup.string().optional().min(3),
+    }),
+  ),
+}));
+
+//create (controller)
 
 export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
-  let validatedData: ICidade | undefined = undefined;
-  try {
-    validatedData = await bodyValidation.validate(req.body, {
-      abortEarly: false,
-    });
-  } catch (err) {
-    const yupError = err as yup.ValidationError;
-    const errors: Record<string, string> = {};
-
-    yupError.inner.forEach((error) => {
-      if (error.path === undefined) return;
-      errors[error.path] = error.message;
-    });
-
-    return res.status(StatusCodes.BAD_REQUEST).json({
-      errors,
-    });
-  }
-
-  console.log(validatedData);
+  console.log(req.body);
 
   return res.send("Create!");
 };
